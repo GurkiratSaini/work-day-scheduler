87bba1b54b37c4e9eaefe40569e584a945d5bd9c
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-22 18:42:37 -0400

    added dynamic jquery elements

---
+++ README.md
@@ -0,0 +1,7 @@
+# Work Day Scheduler
+
+A browser based task scheduler/day planner made with jQuery, Moment.js, Bootstrap, HTML, CSS and JavaScript.
+
+Deployed at: https://gurkiratsaini.github.io/work-day-scheduler
+
+![Home Page](./assets/images/screencapture.png?raw=true)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 var currentDay = document.querySelector("#currentDay");
 var container = document.querySelector(".container");
+var momentEl = moment();
+var containerEl = document.querySelector(".container");
+// console.log(momentEl);
 
-currentDay.textContent = moment().format("dddd, MMM Do");
+currentDay.textContent = momentEl.format("dddd, MMM Do");
+
+function generateTimeBlocks() {
+    for (let i = 0; i < 9; i++) {
+        var rowEl = $("<div>").addClass("row time-block")
+        var timeBlockEl = $("<div>").addClass("col-1 hour");
+        var textareaEl = $("<textarea>").addClass("col-10 description");
+        var saveBtnEl = $("<div>").addClass("col-1 saveBtn").html("<i class='fas fa-save'></i>");
+        $(rowEl).append(timeBlockEl, textareaEl, saveBtnEl);
+        $(".container").append(rowEl);
+    }
+}
+
+generateTimeBlocks();
