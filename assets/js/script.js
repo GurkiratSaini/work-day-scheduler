e19743c11c0b625eaa72369f1111d9fa43dd5ac1
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-24 10:40:53 -0400

    finished application, readme and comments pending

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,44 @@
-// var currentDay = document.querySelector("#currentDay");
-// var container = document.querySelector(".container");
-// var momentEl = moment();
-// var containerEl = document.querySelector(".container");
-// console.log(momentEl);
 
 $("#currentDay").text(moment().format("dddd, MMM Do"));
 
 function generateTimeBlocks() {
     for (let i = 0; i < 9; i++) {
-        var rowEl = $("<div>").addClass("row time-block")
-        var timeBlockEl = $("<div>").addClass("col-1 hour").text(moment().hour(i+9).format("ha").toUpperCase());
-        timeBlockEl.attr("id", `hour-${i+9}`);
-        textareaState(i, $(timeBlockEl).text());
-        var textareaEl = $("<textarea>").addClass("col-10 description");
+        var rowTimeEl = moment().hour(i+9);
+        var rowEl = $("<div>").addClass("row time-block");
+        var timeBlockEl = $("<div>").addClass("col-1 hour").text(rowTimeEl.format("hA"));
+
+        var textareaEl = $("<textarea>").addClass("col-10 description").attr("id", `hour-${i}`);
+
         var saveBtnEl = $("<div>").addClass("col-1 d-flex justify-content-center align-items-center saveBtn").html("<i class='fas fa-save'></i>");
         $(rowEl).append(timeBlockEl, textareaEl, saveBtnEl);
         $(".container").append(rowEl);
+
+        var currentTime = moment();
+
+        if(currentTime.isAfter(rowTimeEl, "hour")){
+            textareaEl.addClass("past");
+        }
+        else if (currentTime.isBefore(rowTimeEl, "hour")){
+            textareaEl.addClass("future");
+        }
+        else {
+            textareaEl.addClass("present");
+        }
+
+        var storageData = localStorage.getItem(`hour-${i}`);
+        if(storageData){
+            $(`#hour-${i}`).val(storageData);
+        }
     }
 }
 
-function textareaState(index, hour) {
-    // background color for the textarea
-    console.log(`${index} index and ${hour} hour`)
+
+
+function saveTextareaContent(event){
+    localStorage.setItem($(this).prev().attr("id"), $(this).prev().val());
 }
 
-function textareaContent(event){
-    console.log(event);
-    
-}
-
-// console.log(moment().format("ha").toUpperCase());
 
 generateTimeBlocks();
-textareaState();
 
-$(".saveBtn").on("click", textareaContent);
-
-/* TO - DO
-    - Add id to row
-    - Set up localStorage
-    - Set up textarea background using momentjs
-*/
+$(".saveBtn").on("click", saveTextareaContent);
